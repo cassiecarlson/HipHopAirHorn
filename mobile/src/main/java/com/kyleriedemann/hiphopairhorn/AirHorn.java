6a9190a3fc7371edfa4c48665ddf4516a4abf528
Kyle Riedemann <kylealanr@gmail.com>
2016-08-21 17:12:06 -0500

    Updated dependencies, fixed disgusting code, fixed circular watch view, play sound from watch speaker

--- mobile/src/main/java/com/kyleriedemann/hiphopairhorn/AirHorn.java
+++ mobile/src/main/java/com/kyleriedemann/hiphopairhorn/AirHorn.java
@@ -2,95 +2,61 @@ package com.kyleriedemann.hiphopairhorn;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
 import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
-import com.readystatesoftware.systembartint.SystemBarTintManager;
-
+import butterknife.BindView;
 
 public class AirHorn extends Activity {
 
     // media player to play the sound
     MediaPlayer airHornPlayer;
 
+    @BindView(R.id.air_horn_button)
+    View afroGuy;
+
+    @BindView(R.id.speaker_left)
+    View leftSpeaker;
+
+    @BindView(R.id.speaker_right)
+    View rightSpeaker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_air_horn);
 
-        // tint the status bar in Kit Kat
-        SystemBarTintManager tintManager = new SystemBarTintManager(this);
-        tintManager.setStatusBarTintEnabled(true);
-        tintManager.setTintColor(Color.parseColor("#DA4453"));
-
         // create an intent that will start the service to listen for the message from the watch
         Intent i = new Intent(this, DataLayerListenerService.class);
         this.startService(i);
 
-        findViewById(R.id.main_view).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                playHorn();
-                scaleImage();
-                return false;
-            }
+        findViewById(R.id.main_view).setOnTouchListener((view, motionEvent) -> {
+            playHorn();
+            scaleImage();
+            return false;
         });
     }
 
     // method to play the animation in anim/pulse.xml
     private void scaleImage() {
-        ImageView imageView = (ImageView) findViewById(R.id.air_horn_button);
-        Animation pulse = AnimationUtils.loadAnimation(this, R.anim.pulse);
-        imageView.startAnimation(pulse);
 
+        Animation pulse = AnimationUtils.loadAnimation(this, R.anim.pulse);
+
+        afroGuy.startAnimation(pulse);
 
         Animation smallPulse = AnimationUtils.loadAnimation(this, R.anim.small_pulse);
-        ImageView imageView1 = (ImageView) findViewById(R.id.speaker_left);
-        imageView1.startAnimation(smallPulse);
 
-        ImageView imageView2 = (ImageView) findViewById(R.id.speaker_right);
-        imageView2.startAnimation(smallPulse);
+        leftSpeaker.startAnimation(smallPulse);
+        rightSpeaker.startAnimation(smallPulse);
     }
 
     // binds the sound to the media player and plays
     private void playHorn() {
         airHornPlayer = MediaPlayer.create(this, R.raw.air_horn);
-        airHornPlayer.setOnCompletionListener(new OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mediaPlayer) {
-                mediaPlayer.release();
-            }
-        });
+        airHornPlayer.setOnCompletionListener(MediaPlayer::release);
         airHornPlayer.start();
-        Log.d("PLAYING THE HORN", "PLAYING THE HORN FROM THE PHONE");
     }
-
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.air_horn, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    */
 }
--- wear/src/main/java/com/kyleriedemann/hiphopairhorn/WearHorn.java
+++ wear/src/main/java/com/kyleriedemann/hiphopairhorn/WearHorn.java
@@ -1,73 +1,118 @@
 package com.kyleriedemann.hiphopairhorn;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.AudioDeviceInfo;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.Node;
 import com.google.android.gms.wearable.NodeApi;
 import com.google.android.gms.wearable.Wearable;
 
 import java.util.List;
 
+import butterknife.BindView;
+
 public class WearHorn extends Activity {
 
-    private GoogleApiClient myGoogleApiClient;
+    private GoogleApiClient googleApiClient;
+
+    private boolean hasSpeaker;
+
+    @BindView(R.id.wear_button)
+    View afroGuy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rect_activity_wear_horn);
 
+        hasSpeaker = hasSpeaker();
+
         // create the google api client to send a message from the watch to the phone
-        myGoogleApiClient = new GoogleApiClient.Builder(this)
+        googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Wearable.API)
                 .build();
-        myGoogleApiClient.connect();
 
-        findViewById(R.id.wear_view).setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                sendMessage();
-                scaleImage();
-                return false;
-            }
-        });
+        googleApiClient.connect();
+
+        findViewById(R.id.wear_view).setOnTouchListener(this::onTouchListener);
+    }
+
+    private boolean onTouchListener(View view, MotionEvent motionEvent) {
+        sendMessage();
+        scaleImage();
+
+        if (hasSpeaker) {
+            playHorn();
+        }
+
+        return false;
     }
 
     // send the message to the watch
     private void sendMessage() {
-        if (myGoogleApiClient == null)
+        if (googleApiClient == null) {
             return;
+        }
 
-        final PendingResult<NodeApi.GetConnectedNodesResult> nodes = Wearable.NodeApi.getConnectedNodes(myGoogleApiClient);
-        nodes.setResultCallback(new ResultCallback<NodeApi.GetConnectedNodesResult>() {
-            @Override
-            public void onResult(NodeApi.GetConnectedNodesResult result) {
-                final List<Node> nodes = result.getNodes();
-                for (int i=0; i<nodes.size(); i++) {
-                    final Node node = nodes.get(i);
+        final PendingResult<NodeApi.GetConnectedNodesResult> nodes =
+                Wearable.NodeApi.getConnectedNodes(googleApiClient);
 
-                    // send the message to the watch
-                    // specify the api client, the node, the message content, and a byte stream array
-                    Wearable.MessageApi.sendMessage(myGoogleApiClient, node.getId(), "/MESSAGE", new byte[]{1});
-                    Log.d("SENDING MESSAGE", "THIS IS A MESSAGE SENT TO THE PHONE FROM THE WATCH");
-                }
-            }
-        });
+        nodes.setResultCallback(this::resultCallback);
+    }
+
+    private void resultCallback(NodeApi.GetConnectedNodesResult result) {
+        final List<Node> nodes1 = result.getNodes();
+        for (int i = 0; i < nodes1.size(); i++) {
+            final Node node = nodes1.get(i);
+
+            // send the message to the watch
+            // specify the api client, the node, the message content, and a byte stream array
+            Wearable.MessageApi.sendMessage(googleApiClient, node.getId(), null, new byte[]{1});
+        }
     }
 
     private void scaleImage() {
-        ImageView imageView = (ImageView) findViewById(R.id.wear_button);
         Animation pulse = AnimationUtils.loadAnimation(this, R.anim.pulse);
-        imageView.startAnimation(pulse);
+
+        afroGuy.startAnimation(pulse);
+    }
+
+    private boolean hasSpeaker() {
+        PackageManager packageManager = this.getPackageManager();
+        AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+
+        // Check whether the device has a speaker.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Check FEATURE_AUDIO_OUTPUT to guard against false positives.
+            if (!packageManager.hasSystemFeature(PackageManager.FEATURE_AUDIO_OUTPUT)) {
+                return false;
+            }
+
+            AudioDeviceInfo[] devices = audioManager.getDevices(AudioManager.GET_DEVICES_OUTPUTS);
+            for (AudioDeviceInfo device : devices) {
+                if (device.getType() == AudioDeviceInfo.TYPE_BUILTIN_SPEAKER) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void playHorn() {
+        MediaPlayer airHornPlayer = MediaPlayer.create(this, R.raw.air_horn);
+        airHornPlayer.setOnCompletionListener(MediaPlayer::release);
+        airHornPlayer.start();
     }
 }
